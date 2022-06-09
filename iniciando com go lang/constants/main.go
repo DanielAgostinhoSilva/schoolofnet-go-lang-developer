e9f2ca075c3bc54abb9d4742b0ae0bba2d653d14
Daniel <daniel.agostinho@hotmail.com>
2022-06-09 14:11:27 -0300

    Entendendo constantes

---
+++ iniciando com go lang/constants/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+const (
+	aa = 55
+	bb = 66
+	cc = 77
+)
+
+const xvz int = 1333
+
+func main() {
+
+	const xpto = 10
+
+	fmt.Printf("%v : %T\n", xpto, xpto)
+	fmt.Printf("%v : %T\n", xvz, xvz)
+
+}
