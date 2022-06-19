fa742b011754a9b076316e6aaa46487911c09446
Daniel <daniel.agostinho@hotmail.com>
2022-06-19 16:03:55 -0300

    Trabalhando com Slices - Parte 2

--- iniciando com go lang/slices/main.go
+++ iniciando com go lang/slices/main.go
@@ -3,10 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	slice := make([]int, 2)
-	slice = append(slice, 1, 2, 3, 5)
+	slice := make([]int, 2, 5)
+	//slice = append(slice, 1, 2, 3, 5)
 
 	fmt.Println(slice)
 	fmt.Println(len(slice))
 	fmt.Println(cap(slice))
+
+	//for i := 0; i < 20; i++ {
+	//	slice = append(slice, i)
+	//	fmt.Println("Len: ", len(slice), " Cap: ", cap(slice))
+	//}
+
+	sliceTest := slice
+	slice = append(slice, 1, 2, 3, 4)
+	slice[0] = 10
+	fmt.Println(slice)
+	fmt.Println(sliceTest)
 }
